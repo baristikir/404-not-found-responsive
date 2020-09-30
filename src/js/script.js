548aa8d9b53107ece9b8e7b774319636ce79dcdb
Baris Tikir <brstkr@icloud.com>
2020-09-30 20:40:42 +0200

    Styling Finished + Dark Mode Added :package:

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // * JS Script Goes Here * //
+document.onkeypress = function(e) {
+    e = e || window.event;
+
+    if(e.keyCode === 13) {
+      document.documentElement.classList.toggle('dark-mode');
+
+      document.querySelectorAll('.inverted').forEach(result => {
+        result.classList.toggle('invert');
+      })
+    }
+}
